336bd7647df756a86122cbbf095fafd9824dfe06
Minh Tuan <tuannm098@gmail.com>
2022-08-16 08:57:45 +0700

    bt

---
+++ HW-Lesson7/HW-Lesson7.js
@@ -0,0 +1,16 @@
+//bt slide
+let greeting = "Hello. My name is John";
+console.log(greeting.charAt(8));
+
+let str1 = "the morning is upon us.";
+console.log(str1.slice(12, 22));
+
+let str2 = "Hello";
+let str3 = "World";
+console.log(str2.concat(" ", str3));
+
+//btmt
+let string1="Hôm nay ngày thứ 7";
+let string2= string1.toLowerCase();
+let string3= string2.replace (7,8);
+console.log('Result:', string3.slice(0,3) + string3.slice(4));
